b79c1ed82226094f8e85cacb20e0cf9bcaf6dc50
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-04-19 15:24:55 -0300

    liftoff

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Welcome } from './src/pages/welcome';
 
 export default function App() {
   return (
-    <Text>
-
-    </Text>
+    <Welcome />
   )
 }
+
